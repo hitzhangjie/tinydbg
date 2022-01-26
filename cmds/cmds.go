28a63bba3a542cc7eb0b57b9b397ef785033ad21
hitzhangjie <hit.zhangjie@gmail.com>
2022-01-26 14:28:21 +0800

    refactor: move func `connect` to cmds.go

--- cmds/cmds.go
+++ cmds/cmds.go
@@ -2,6 +2,7 @@ package cmds
 
 import (
 	"fmt"
+	"github.com/hitzhangjie/dlv/pkg/terminal"
 	"net"
 	"os"
 	"os/signal"
@@ -175,4 +176,35 @@ func splitArgs(cmd *cobra.Command, args []string) ([]string, []string) {
 		return args[:cmd.ArgsLenAtDash()], args[cmd.ArgsLenAtDash():]
 	}
 	return args, []string{}
+}
+
+func connect(addr string, clientConn net.Conn, conf *config.Config, kind debugger.ExecuteKind) int {
+	// Create and start a terminal - attach to running instance
+	var client *rpcv2.RPCClient
+	if clientConn != nil {
+		client = rpcv2.NewClientFromConn(clientConn)
+	} else {
+		client = rpcv2.NewClient(addr)
+	}
+	if client.IsMulticlient() {
+		state, _ := client.GetStateNonBlocking()
+		// The error return of GetState will usually be the ErrProcessExited,
+		// which we don't care about. If there are other errors they will show up
+		// later, here we are only concerned about stopping a running target so
+		// that we can initialize our connection.
+		if state != nil && state.Running {
+			_, err := client.Halt()
+			if err != nil {
+				log.Error("could not halt: %v", err)
+				return 1
+			}
+		}
+	}
+	term := terminal.New(client, conf)
+	term.InitFile = initFile
+	status, err := term.Run()
+	if err != nil {
+		log.Error("%v", err)
+	}
+	return status
 }
--- cmds/connect.go
+++ cmds/connect.go
@@ -3,16 +3,12 @@ package cmds
 import (
 	"errors"
 	"fmt"
-	"net"
 	"os"
 
 	"github.com/spf13/cobra"
 
 	"github.com/hitzhangjie/dlv/pkg/config"
-	"github.com/hitzhangjie/dlv/pkg/log"
-	"github.com/hitzhangjie/dlv/pkg/terminal"
 	"github.com/hitzhangjie/dlv/service/debugger"
-	"github.com/hitzhangjie/dlv/service/rpcv2"
 )
 
 // 'connect' subcommand.
@@ -45,35 +41,4 @@ func connectCmdRun(cmd *cobra.Command, args []string) {
 		os.Exit(1)
 	}
 	os.Exit(connect(addr, nil, conf, debugger.ExecutingOther))
-}
-
-func connect(addr string, clientConn net.Conn, conf *config.Config, kind debugger.ExecuteKind) int {
-	// Create and start a terminal - attach to running instance
-	var client *rpcv2.RPCClient
-	if clientConn != nil {
-		client = rpcv2.NewClientFromConn(clientConn)
-	} else {
-		client = rpcv2.NewClient(addr)
-	}
-	if client.IsMulticlient() {
-		state, _ := client.GetStateNonBlocking()
-		// The error return of GetState will usually be the ErrProcessExited,
-		// which we don't care about. If there are other errors they will show up
-		// later, here we are only concerned about stopping a running target so
-		// that we can initialize our connection.
-		if state != nil && state.Running {
-			_, err := client.Halt()
-			if err != nil {
-				log.Error("could not halt: %v", err)
-				return 1
-			}
-		}
-	}
-	term := terminal.New(client, conf)
-	term.InitFile = initFile
-	status, err := term.Run()
-	if err != nil {
-		log.Error("%v", err)
-	}
-	return status
 }
