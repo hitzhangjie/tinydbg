d489540f14f51d451343fde83a4bbe3bacf9d215
hitzhangjie <hit.zhangjie@gmail.com>
2022-01-26 19:13:14 +0800

    delete support for arm64

--- pkg/proc/proc_test.go
+++ pkg/proc/proc_test.go
@@ -4163,7 +4163,6 @@ func TestReadDeferArgs(t *testing.T) {
 
 func TestIssue1374(t *testing.T) {
 	// Continue did not work when stopped at a breakpoint immediately after calling CallFunction.
-	protest.MustSupportFunctionCalls(t, testBackend)
 	withTestProcess("issue1374", t, func(p *proc.Target, fixture protest.Fixture) {
 		setFileBreakpoint(p, t, fixture.Source, 7)
 		assertNoError(p.Continue(), t, "First Continue")
@@ -4393,8 +4392,6 @@ func testCallConcurrentCheckReturns(p *proc.Target, t *testing.T, gid1, gid2 int
 }
 
 func TestCallConcurrent(t *testing.T) {
-	skipOn(t, "broken", "freebsd")
-	protest.MustSupportFunctionCalls(t, testBackend)
 	withTestProcess("teststepconcurrent", t, func(p *proc.Target, fixture protest.Fixture) {
 		bp := setFileBreakpoint(p, t, fixture.Source, 24)
 		assertNoError(p.Continue(), t, "Continue()")
@@ -4632,7 +4629,6 @@ func TestIssue1925(t *testing.T) {
 	// In particular the stepInstructionOut function called at the end of a
 	// 'call' procedure should clean the G cache like every other function
 	// altering the state of the target process.
-	protest.MustSupportFunctionCalls(t, testBackend)
 	withTestProcess("testvariables2", t, func(p *proc.Target, fixture protest.Fixture) {
 		assertNoError(p.Continue(), t, "Continue()")
 		assertNoError(proc.EvalExpressionWithCalls(p, p.SelectedGoroutine(), "afunc(2)", normalLoadConfig, true), t, "Call")
