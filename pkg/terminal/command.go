6a23e62deeae3de4052077052ac6146cb69c42dc
hitzhangjie <hit.zhangjie@gmail.com>
2025-06-08 12:03:25 +0800

    tinydbg/terminal: refactor the code

--- pkg/terminal/command.go
+++ pkg/terminal/command.go
@@ -2825,3 +2825,24 @@ func shouldAskToSuspendBreakpoint(t *Term) bool {
 	_, err := t.client.GetState()
 	return len(fns) > 0 || isErrProcessExited(err) || t.client.FollowExecEnabled()
 }
+
+func disasmPrint(dv api.AsmInstructions, out io.Writer, showHeader bool) {
+	bw := bufio.NewWriter(out)
+	defer bw.Flush()
+	if len(dv) > 0 && dv[0].Loc.Function != nil && showHeader {
+		fmt.Fprintf(bw, "TEXT %s(SB) %s\n", dv[0].Loc.Function.Name(), dv[0].Loc.File)
+	}
+	tw := tabwriter.NewWriter(bw, 1, 8, 1, '\t', 0)
+	defer tw.Flush()
+	for _, inst := range dv {
+		atbp := ""
+		if inst.Breakpoint {
+			atbp = "*"
+		}
+		atpc := ""
+		if inst.AtPC {
+			atpc = "=>"
+		}
+		fmt.Fprintf(tw, "%s\t%s:%d\t%#x%s\t%x\t%s\n", atpc, filepath.Base(inst.Loc.File), inst.Loc.Line, inst.Loc.PC, atbp, inst.Bytes, inst.Text)
+	}
+}
