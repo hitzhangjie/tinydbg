4a421ac9a2bc584d9484c7829c42e8ef9340ef7d
hitzhangjie <hit.zhangjie@gmail.com>
2025-06-08 18:58:20 +0800

    tinydbg/rpc: refactor the registered rpc methods

--- service/rpccommon/server.go
+++ service/rpccommon/server.go
@@ -108,8 +108,7 @@ func (s *ServerImpl) Run() error {
 	s.s2 = rpc2.NewServer(s.config, s.debugger)
 	s.methodMap = make(map[string]*methodType)
 
-	suitableMethods2(s.s2, s.methodMap)
-	finishMethodsMapInit(s.methodMap)
+	registerMethods(s.s2, s.methodMap)
 
 	go func() {
 		defer s.listener.Close()
@@ -143,18 +142,6 @@ func (s *ServerImpl) serveConnection(c io.ReadWriteCloser) {
 	conn := &bufReadWriteCloser{bufio.NewReader(c), c}
 	s.log.Debugf("serving JSON-RPC on new connection")
 	go s.serveJSONCodec(conn)
-}
-
-func finishMethodsMapInit(methods map[string]*methodType) {
-	for name, method := range methods {
-		mtype := method.method.Type()
-		if mtype.NumIn() != 2 {
-			panic(fmt.Errorf("wrong number of inputs for method %s (%d)", name, mtype.NumIn()))
-		}
-		method.ArgType = mtype.In(0)
-		method.ReplyType = mtype.In(1)
-		method.Synchronous = method.ReplyType.String() != "service.RPCCallback"
-	}
 }
 
 func (s *ServerImpl) serveJSONCodec(conn io.ReadWriteCloser) {
@@ -357,4 +344,62 @@ func (err *internalError) Error() string {
 		fmt.Fprintf(&out, "%s (%#x)\n\t%s:%d\n", frame.Func, frame.Pc, frame.File, frame.Line)
 	}
 	return out.String()
+}
+
+func registerMethods(s *rpc2.RPCServer, methods map[string]*methodType) {
+	defer func() {
+		for name, method := range methods {
+			mtype := method.method.Type()
+			if mtype.NumIn() != 2 {
+				panic(fmt.Errorf("wrong number of inputs for method %s (%d)", name, mtype.NumIn()))
+			}
+			method.ArgType = mtype.In(0)
+			method.ReplyType = mtype.In(1)
+			method.Synchronous = method.ReplyType.String() != "service.RPCCallback"
+		}
+	}()
+	methods["RPCServer.AmendBreakpoint"] = &methodType{method: reflect.ValueOf(s.AmendBreakpoint)}
+	methods["RPCServer.Ancestors"] = &methodType{method: reflect.ValueOf(s.Ancestors)}
+	methods["RPCServer.AttachedToExistingProcess"] = &methodType{method: reflect.ValueOf(s.AttachedToExistingProcess)}
+	methods["RPCServer.BuildID"] = &methodType{method: reflect.ValueOf(s.BuildID)}
+	methods["RPCServer.CancelNext"] = &methodType{method: reflect.ValueOf(s.CancelNext)}
+	methods["RPCServer.ClearBreakpoint"] = &methodType{method: reflect.ValueOf(s.ClearBreakpoint)}
+	methods["RPCServer.Command"] = &methodType{method: reflect.ValueOf(s.Command)}
+	methods["RPCServer.CreateBreakpoint"] = &methodType{method: reflect.ValueOf(s.CreateBreakpoint)}
+	methods["RPCServer.CreateWatchpoint"] = &methodType{method: reflect.ValueOf(s.CreateWatchpoint)}
+	methods["RPCServer.Detach"] = &methodType{method: reflect.ValueOf(s.Detach)}
+	methods["RPCServer.Disassemble"] = &methodType{method: reflect.ValueOf(s.Disassemble)}
+	methods["RPCServer.DumpCancel"] = &methodType{method: reflect.ValueOf(s.DumpCancel)}
+	methods["RPCServer.DumpStart"] = &methodType{method: reflect.ValueOf(s.DumpStart)}
+	methods["RPCServer.DumpWait"] = &methodType{method: reflect.ValueOf(s.DumpWait)}
+	methods["RPCServer.Eval"] = &methodType{method: reflect.ValueOf(s.Eval)}
+	methods["RPCServer.ExamineMemory"] = &methodType{method: reflect.ValueOf(s.ExamineMemory)}
+	methods["RPCServer.FindLocation"] = &methodType{method: reflect.ValueOf(s.FindLocation)}
+	methods["RPCServer.FollowExec"] = &methodType{method: reflect.ValueOf(s.FollowExec)}
+	methods["RPCServer.FollowExecEnabled"] = &methodType{method: reflect.ValueOf(s.FollowExecEnabled)}
+	methods["RPCServer.FunctionReturnLocations"] = &methodType{method: reflect.ValueOf(s.FunctionReturnLocations)}
+	methods["RPCServer.GetBreakpoint"] = &methodType{method: reflect.ValueOf(s.GetBreakpoint)}
+	methods["RPCServer.GetThread"] = &methodType{method: reflect.ValueOf(s.GetThread)}
+	methods["RPCServer.GuessSubstitutePath"] = &methodType{method: reflect.ValueOf(s.GuessSubstitutePath)}
+	methods["RPCServer.IsMulticlient"] = &methodType{method: reflect.ValueOf(s.IsMulticlient)}
+	methods["RPCServer.ListBreakpoints"] = &methodType{method: reflect.ValueOf(s.ListBreakpoints)}
+	methods["RPCServer.ListDynamicLibraries"] = &methodType{method: reflect.ValueOf(s.ListDynamicLibraries)}
+	methods["RPCServer.ListFunctionArgs"] = &methodType{method: reflect.ValueOf(s.ListFunctionArgs)}
+	methods["RPCServer.ListFunctions"] = &methodType{method: reflect.ValueOf(s.ListFunctions)}
+	methods["RPCServer.ListGoroutines"] = &methodType{method: reflect.ValueOf(s.ListGoroutines)}
+	methods["RPCServer.ListLocalVars"] = &methodType{method: reflect.ValueOf(s.ListLocalVars)}
+	methods["RPCServer.ListPackageVars"] = &methodType{method: reflect.ValueOf(s.ListPackageVars)}
+	methods["RPCServer.ListPackagesBuildInfo"] = &methodType{method: reflect.ValueOf(s.ListPackagesBuildInfo)}
+	methods["RPCServer.ListRegisters"] = &methodType{method: reflect.ValueOf(s.ListRegisters)}
+	methods["RPCServer.ListSources"] = &methodType{method: reflect.ValueOf(s.ListSources)}
+	methods["RPCServer.ListTargets"] = &methodType{method: reflect.ValueOf(s.ListTargets)}
+	methods["RPCServer.ListThreads"] = &methodType{method: reflect.ValueOf(s.ListThreads)}
+	methods["RPCServer.ListTypes"] = &methodType{method: reflect.ValueOf(s.ListTypes)}
+	methods["RPCServer.ProcessPid"] = &methodType{method: reflect.ValueOf(s.ProcessPid)}
+	methods["RPCServer.Restart"] = &methodType{method: reflect.ValueOf(s.Restart)}
+	methods["RPCServer.Set"] = &methodType{method: reflect.ValueOf(s.Set)}
+	methods["RPCServer.Stacktrace"] = &methodType{method: reflect.ValueOf(s.Stacktrace)}
+	methods["RPCServer.State"] = &methodType{method: reflect.ValueOf(s.State)}
+	methods["RPCServer.StopRecording"] = &methodType{method: reflect.ValueOf(s.StopRecording)}
+	methods["RPCServer.ToggleBreakpoint"] = &methodType{method: reflect.ValueOf(s.ToggleBreakpoint)}
 }
